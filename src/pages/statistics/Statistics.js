077431c07100e8504eea0b0eda427d181c477ba8
vlad <swebus999@gmail.com>
2020-12-08 16:46:08 +0300

    Add statistics page and settings page

--- src/pages/statistics/Statistics.js
+++ src/pages/statistics/Statistics.js
@@ -1,22 +1,51 @@
 import React, {Component} from 'react';
 import ProceedsGraph from "../dashboard/proceeds/ProceedsGraph";
+import BookedGraph from "../dashboard/proceeds/BookedGraph";
 import ArrivalsGraph from "../dashboard/arrivals_graph/ArrivalsGraph";
 import VisitsGraph from "../dashboard/visits/VisitsGraph";
 import "./statistics.css"
+import ApiCalendar from '../calendar/ApiCalendar.js';
 
+var DATA = [[
+    {
+        x: 'Nov 9, 2020',
+        y: 10000
+    }
+]];
 class Statistics extends Component {
+
     render() {
+
+
+   var date = new Date();
+   var start =  new Date(date.getFullYear(), date.getMonth(), 0);
+   var end = new Date(date.getFullYear(), date.getMonth() + 1, -1);
+console.log(start);
+          var loop = new Date(start);
+            var nnn = 0;
+          while(loop <= end){
+            nnn = nnn+1
+            var newDate = loop.setDate(loop.getDate() + 1);
+    ApiCalendar.listBookedEvents(nnn,new Date(newDate), 50)
+            loop = new Date(newDate);
+          }
+
         return (
-            <div className={'statistics'}>
-                <h1 className={"stat_title"}>Statistics</h1>
+            <div className = "main-block">
+            <div className = "s-title">Статистика</div>
+    <hr class="separator" />
+
 
                 <div className={"stat_proceeds"}>
-                    <ProceedsGraph/>
+                  <ProceedsGraph/>
+                  <BookedGraph/>
+
                 </div>
                 <div className={"mini_graphs"}>
-                    <ArrivalsGraph/>
-                    <VisitsGraph/>
+                <ArrivalsGraph/>
+                <VisitsGraph/>
                 </div>
+
             </div>
         );
     }
